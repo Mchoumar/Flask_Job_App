892d3c584b249d79f1638f96242121583441d618
choumar2001 <65606795+choumar2001@users.noreply.github.com>
2023-04-09 11:14:45 +0200

    Add database table data

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
@@ -14,7 +15,7 @@ db = SQLAlchemy(app)
 
 
 class Form(db.Model):
-    """Setup a table for the database file"""
+    """Set up a table for the database file"""
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(80))
     last_name = db.Column(db.String(80))
@@ -31,10 +32,25 @@ def index():
         last_name = request.form["last_name"]
         email = request.form["email"]
         date = request.form["date"]
+        date_obj = datetime.strptime(date, "%Y-%m-%d")
         occupation = request.form["occupation"]
+
+        # Add the data into the table
+        form = Form(first_name=first_name, last_name=last_name, email=email,
+                    date=date_obj, occupation=occupation)
+
+        # Inserts the data into the database file
+        db.session.add(form)
+        db.session.commit()
 
     # Renders the html file
     return render_template("index.html")
 
 
-app.run(debug=True, port=5001)
+if __name__ == '__main__':
+    # Creates the database
+    with app.app_context():
+        db.create_all()
+
+    # Starts the debugging process for the flask app
+    app.run(debug=True, port=5001)
